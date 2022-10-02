c23d58e07bdf1d8c86ac6e6f976475f944c2b3ff
Ahmed Khanzada <me@enzu.ru>
2022-10-02 19:26:26 -0400

    Initial commit

---
+++ scheme/leap/leap.scm
@@ -0,0 +1,8 @@
+(import (rnrs))
+
+(define (leap-year? year)
+  (if (equal? (mod year 4) 0)
+      (if (equal? (mod year 100) 0)
+          (if (equal? (mod year 400) 0) #T #F)
+          #T)
+      #F))
