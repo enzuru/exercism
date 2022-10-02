35e8d6225c3437ea4d842a26c7964ead9f48f0ef
Ahmed Khanzada <me@enzu.ru>
2022-10-02 19:36:28 -0400

    Make more stylistically interesting

--- scheme/leap/leap.scm
+++ scheme/leap/leap.scm
@@ -3,6 +3,8 @@
 (define (leap-year? year)
   (if (equal? (mod year 4) 0)
       (if (equal? (mod year 100) 0)
-          (if (equal? (mod year 400) 0) #T #F)
+          (if (equal? (mod year 400) 0)
+              #T
+              #F)
           #T)
       #F))
