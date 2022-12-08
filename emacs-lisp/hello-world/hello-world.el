bd3d722f1d459d227ca1da19db05a4bb1af130df
Ahmed Khanzada <me@enzu.ru>
2022-12-08 10:53:13 -0500

    Added a couple more languages

---
+++ emacs-lisp/hello-world/hello-world.el
@@ -0,0 +1,9 @@
+;;; hello-world.el --- Hello World exercise (exercism)  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+(defun hello ()
+  "Hello, World!")
+
+(provide 'hello-world)
+;;; hello-world.el ends here
